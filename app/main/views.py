9a3c23d6f120befcdacea1e02fc98a03deed2212
Annetkhavere <anneywan5@gmail.com>
2022-05-02 17:57:56 +0300

    update views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
 from flask import render_template
-from .. import main
+from . import main
+from ..request import get_news, get_sources
+
 
 #views
 @main.route('/')
@@ -7,4 +9,13 @@ def index():
   '''
   View root page function that returns the index page and its data
   '''
-  pass
+  news = get_news() 
+  title = 'NEWS'
+  return render_template('index.html', title = title,articles = news)
+@main.route('/sources')
+def sources():
+  '''
+  View news page function that returns the news details page and its data
+  '''
+  news = get_sources()
+  return render_template('news.html',sources = news)
