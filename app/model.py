b6fbf48787473d381366ee718129b79cb77087f1
Annetkhavere <anneywan5@gmail.com>
2022-05-02 16:35:46 +0300

    create a class called news

---
+++ app/model.py
@@ -0,0 +1,12 @@
+class News:
+  '''
+  News class to define News objects
+  '''
+
+  def __init__(self, title, description,urlToImage,content,publishedAt):
+    self.title = title
+    self.description = description
+    self.urlToImage = urlToImage
+    self.content = content
+    self.publishedAt = publishedAt
+
