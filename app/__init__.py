25e807524dde30294fc23cfc346c380ec85707d3
Annetkhavere <anneywan5@gmail.com>
2022-05-03 10:36:27 +0300

    rename model.py to models.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from app import config_options
+from config import config_options
 
 bootstrap = Bootstrap()
 
--- app/request.py
+++ app/request.py
@@ -1,97 +1,367 @@
+from .models import Articles
+from .models import Sources
+from newsapi import NewsApiClient
+from .config import Config
 import urllib.request,json
-from .model import News, Sources
 
-#Getting API key
-api_key = None
-#Getting  the news base url
-base_url = None
+api_key=None
+base_url=None
+base_url_for_everything=None
+base_url_top_headlines=None
+base_source_list=None
 
-def configure_request(app):
-  global api_key, base_url
-  api_key = app.config['NEWS_API_KEY']
-  base_url = app.config['NEWS_API_BASE_URL']
-  
-def get_news():
+def publishedArticles():
+    newsapi = NewsApiClient(api_key= Config.API_KEY)
+
+    get_articles = newsapi.get_everything(sources= 'cnn, reuters, cnbc, the-verge, gizmodo, the-next-web, techradar, recode, ars-technica')
+
+    all_articles = get_articles['articles']
+
+    articles_results = []
+
+    source = []
+    title = []
+    desc = []
+    author = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(all_articles)):
+        article = all_articles[i]
+
+        source.append(article['source'])
+        title.append(article['title'])
+        desc.append(article['description'])
+        author.append(article['author'])
+        img.append(article['urlToImage'])
+        p_date.append(article['publishedAt'])
+        url.append(article['url'])
+
+        article_object = Articles(source, title, desc, author, img, p_date, url)
+
+        articles_results.append(article_object)
+
+        contents = zip(source, title, desc, author, img, p_date, url)
+
+    return  contents
+
+def topHeadlines():
+    newsapi = NewsApiClient(api_key= Config.API_KEY)
+
+    top_headlines = newsapi.get_top_headlines(sources= 'cnn, reuters, cnbc, techcrunch, the-verge, gizmodo, the-next-web, techradar, recode, ars-technica')
+
+    all_headlines = top_headlines['articles']
+
+    articles_results = []
+
+    source = []
+    title = []
+    desc = []
+    author = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(all_headlines)):
+        headline = all_headlines[i]
+
+        source.append(headline['source'])
+        title.append(headline['title'])
+        desc.append(headline['description'])
+        author.append(headline['author'])
+        img.append(headline['urlToImage'])
+        p_date.append(headline['publishedAt'])
+        url.append(headline['url'])
+
+        article_object = Articles(source, title, desc, author, img, p_date, url)
+
+        articles_results.append(article_object)
+
+        contents = zip(source, title, desc, author, img, p_date, url)
+
+    return  contents
+
+def randomArticles():
+    newsapi = NewsApiClient(api_key= Config.API_KEY)
+
+    random_articles = newsapi.get_everything(sources= 'the-verge, gizmodo, the-next-web, recode, ars-technica')
+
+    all_articles = random_articles['articles']
+
+    articles_results = []
+
+    source = []
+    title = []
+    desc = []
+    author = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(all_articles)):
+        article = all_articles[i]
+
+        source.append(article['source'])
+        title.append(article['title'])
+        desc.append(article['description'])
+        author.append(article['author'])
+        img.append(article['urlToImage'])
+        p_date.append(article['publishedAt'])
+        url.append(article['url'])
+
+        article_object = Articles(source, title, desc, author, img, p_date, url)
+
+        articles_results.append(article_object)
+
+        contents = zip(source, title, desc, author, img, p_date, url)
+
+    return  contents
+
+def businessArticles():
+    newsapi = NewsApiClient(api_key= Config.API_KEY)
+
+    business_articles = newsapi.get_top_headlines(category='business')
+
+    all_articles = business_articles['articles']
+
+    business_articles_results = []
+
+    source = []
+    title = []
+    desc = []
+    author = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(all_articles)):
+        article = all_articles[i]
+
+        source.append(article['source'])
+        title.append(article['title'])
+        desc.append(article['description'])
+        author.append(article['author'])
+        img.append(article['urlToImage'])
+        p_date.append(article['publishedAt'])
+        url.append(article['url'])
+
+        article_object = Articles(source, title, desc, author, img, p_date, url)
+
+        business_articles_results.append(article_object)
+
+        contents = zip(source, title, desc, author, img, p_date, url)
+
+    return  contents
+
+def techArticles():
+    newsapi = NewsApiClient(api_key= Config.API_KEY)
+
+    tech_articles = newsapi.get_top_headlines(category='technology')
+
+    all_articles = tech_articles['articles']
+
+    tech_articles_results = []
+
+    source = []
+    title = []
+    desc = []
+    author = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(all_articles)):
+        article = all_articles[i]
+
+        source.append(article['source'])
+        title.append(article['title'])
+        desc.append(article['description'])
+        author.append(article['author'])
+        img.append(article['urlToImage'])
+        p_date.append(article['publishedAt'])
+        url.append(article['url'])
+
+        article_object = Articles(source, title, desc, author, img, p_date, url)
+
+        tech_articles_results.append(article_object)
+
+        contents = zip(source, title, desc, author, img, p_date, url)
+
+    return  contents
+
+def entArticles():
+    newsapi = NewsApiClient(api_key= Config.API_KEY)
+
+    ent_articles = newsapi.get_top_headlines(category='entertainment')
+
+    all_articles = ent_articles['articles']
+
+    ent_articles_results = []
+
+    source = []
+    title = []
+    desc = []
+    author = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(all_articles)):
+        article = all_articles[i]
+
+        source.append(article['source'])
+        title.append(article['title'])
+        desc.append(article['description'])
+        author.append(article['author'])
+        img.append(article['urlToImage'])
+        p_date.append(article['publishedAt'])
+        url.append(article['url'])
+
+        article_object = Articles(source, title, desc, author, img, p_date, url)
+
+        ent_articles_results.append(article_object)
+
+        contents = zip(source, title, desc, author, img, p_date, url)
+
+    return  contents
+
+def scienceArticles():
+    newsapi = NewsApiClient(api_key= Config.API_KEY)
+
+    science_articles = newsapi.get_top_headlines(category='science')
+
+    all_articles = science_articles['articles']
+
+    science_articles_results = []
+
+    source = []
+    title = []
+    desc = []
+    author = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(all_articles)):
+        article = all_articles[i]
+
+        source.append(article['source'])
+        title.append(article['title'])
+        desc.append(article['description'])
+        author.append(article['author'])
+        img.append(article['urlToImage'])
+        p_date.append(article['publishedAt'])
+        url.append(article['url'])
+
+        article_object = Articles(source, title, desc, author, img, p_date, url)
+
+        science_articles_results.append(article_object)
+
+        contents = zip(source, title, desc, author, img, p_date, url)
+
+    return  contents
+
+def sportArticles():
+    newsapi = NewsApiClient(api_key= Config.API_KEY)
+
+    sport_articles = newsapi.get_top_headlines(category='sports')
+
+    all_articles = sport_articles['articles']
+
+    sport_articles_results = []
+
+    source = []
+    title = []
+    desc = []
+    author = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(all_articles)):
+        article = all_articles[i]
+
+        source.append(article['source'])
+        title.append(article['title'])
+        desc.append(article['description'])
+        author.append(article['author'])
+        img.append(article['urlToImage'])
+        p_date.append(article['publishedAt'])
+        url.append(article['url'])
+
+        article_object = Articles(source, title, desc, author, img, p_date, url)
+
+        sport_articles_results.append(article_object)
+
+        contents = zip(source, title, desc, author, img, p_date, url)
+
+    return  contents
+
+def healthArticles():
+    newsapi = NewsApiClient(api_key= Config.API_KEY)
+
+    health_articles = newsapi.get_top_headlines(category='health')
+
+    all_articles = health_articles['articles']
+
+    health_articles_results = []
+
+    source = []
+    title = []
+    desc = []
+    author = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(all_articles)):
+        article = all_articles[i]
+
+        source.append(article['source'])
+        title.append(article['title'])
+        desc.append(article['description'])
+        author.append(article['author'])
+        img.append(article['urlToImage'])
+        p_date.append(article['publishedAt'])
+        url.append(article['url'])
+
+        article_object = Articles(source, title, desc, author, img, p_date, url)
+
+        health_articles_results.append(article_object)
+
+        contents = zip(source, title, desc, author, img, p_date, url)
+
+    return  contents
+
+def get_news_source():
   '''
   Function that gets the json response to our url request
   '''
-  get_news_url = base_url.format(api_key)
-  with urllib.urlopen(get_news_url) as url:
-    get_news_data = url.read()
-    get_news_response = json.loads(get_news_data)
+  get_news_source_url = 'https://newsapi.org/v2/sources?apiKey=' + Config.API_KEY
+  with urllib.request.urlopen(get_news_source_url) as url:
+    get_news_source_data = url.read()
+    get_news_source_response = json.loads(get_news_source_data)
 
-    news_results = None
+    news_source_results = None
 
-    if get_news_response['articles']:
-      news_results_list = get_news_response['articles']
-      
-      #print(news_results_list)
-      news_results = process_results(news_results_list)
-      return news_results
+    if get_news_source_response['sources']:
+      news_source_results_list = get_news_source_response['sources']
+      news_source_results = process_sources(news_source_results_list)
 
-def process_results(news_list):
+  return news_source_results
+
+def process_sources(source_list):
   '''
-  Function  that processes the news results and transform them to a list of Objects
-    Args:
-        news_list: A list of dictionaries that contain news details
-    Returns :
-        news_results: A list of news objects
+  function that process the news articles and transform them to a list of objects
   '''
-  news_results = []
-  for news_item in news_list:
-    title = news_item.get('title')
-    description = news_item.get('description')
-    urlToImage = news_item.get('urlToImage')
-    content = news_item.get('content')
-    publishedAt = news_item.get('publishedAt')
+  news_source_result = []
+  for news_source_item in source_list:
+    name = news_source_item.get('name')
+    description = news_source_item.get('description')
+    url = news_source_item.get('url')
 
-    news_object = News(title, description, urlToImage, content, publishedAt)
-    #print(news_object)
-    news_results.append(news_object)
-    #print(news_results)
-
-#sources
-def get_sources():
-    '''
-    Function that gets the json response to our url request
-    '''
-    # get_news_url = base_url.format(api_key)
-    get_sources_url = 'GET https://newsapi.org/v2/top-headlines/sources?apiKey=a4f8556008db4415bc89dfcbd9fd06c4'
-
-    with urllib.request.urlopen(get_sources_url) as url:
-        get_sources_data = url.read()
-        get_sources_response = json.loads(get_sources_data)
-        
-
-        sources_results = None
-        
-        if get_sources_response['sources']:
-            sources_results_list = get_sources_response['sources']
-            # print(news_results_list)
-            sources_results = process_results(sources_results_list)
-
-    return sources_results
-
-def process_results(sources_list):
-    '''
-    Function  that processes the news result and transform them to a list of Objects
-    Args:
-        news_list: A list of dictionaries that contain news details
-    Returns :
-        news_results: A list of news objects
-    '''
-    sources_results = []
-    for sources_item in sources_list:
-        name = sources_item.get('name')
-        description = sources_item.get('description')
-        url = sources_item.get('url')
-       
-
-        
-        sources_object = Sources(name,description,url)
-        #print(sources_object)
-        sources_results.append(sources_object)
-        # print(news_results)
-
-    return sources_results
-
+    if name:
+      news_source_object = Sources(name, description,url)
+      news_source_result.append(news_source_object)
+  return news_source_result
